497515b46aa4e2aa5d6be5a805860d151ab64ccd
Victor Essien <viktoressien1@gmail.com>
2024-03-29 02:33:57 +0100

    Edit

--- frontend/src/pages/CourseSlides.js
+++ frontend/src/pages/CourseSlides.js
@@ -5,8 +5,7 @@ import PopUp from "../components/PopUp";
 import { useSelector, useDispatch } from "react-redux";
 
 const CourseSlides = () => {
- 
-  const files = useSelector((state)=>state.options.files)
+  const files = useSelector((state) => state.options.files);
   const loading = useSelector((state) => state.options.loading);
   const popupState = useSelector((state) => state.options.popup_state);
   const handleLoadingChange = (value) => {
@@ -18,20 +17,20 @@ const CourseSlides = () => {
 
   const handleSetFile = (value) => {
     dispatch({
-      type:"SET_FILE",
-      files:  value
-    })
-  }
-  const maxLength = 27
+      type: "SET_FILE",
+      files: value,
+    });
+  };
+  const maxLength = 27;
   const dispatch = useDispatch();
   const getFiles = async () => {
     handleLoadingChange(true);
     try {
       const res = await fetch("http://localhost:4000/api/file");
       const data = await res.json();
-     
+
       console.log(data.files);
-      handleSetFile(data.files)
+      handleSetFile(data.files);
       handleLoadingChange(false);
     } catch (error) {
       console.log(error);
@@ -84,99 +83,102 @@ const CourseSlides = () => {
           <LoadingSpinner />
         ) : (
           <div>
-             {files &&
-          files.map((file) => {
-            if (file.category === "Chemistry") {
-              return (
-                <div className="file" key={file._id}>
-                <div className="file-sub-cont">
-                  <h3>{file.filename.length > maxLength? file.filename.slice(0,maxLength -3) + '...':file.filename}</h3>
-                  <button
-                    className="download-btn"
-                    onClick={() =>
-                      downloadFile(file._id, file.filename)
-                    }
-                  >
-                    <span class="material-symbols-outlined">
-                      download
-                    </span>
-                  </button>
-                </div>
-              </div>
-              );
-            }
-            return null;
-          })}
+            {files &&
+              files.map((file) => {
+                if (file.category === "Chemistry") {
+                  return (
+                    <div className="file" key={file._id}>
+                      <div className="file-sub-cont">
+                        <h3>
+                          {file.filename.length > maxLength
+                            ? file.filename.slice(0, maxLength - 3) + "..."
+                            : file.filename}
+                        </h3>
+                        <button
+                          className="download-btn"
+                          onClick={() => downloadFile(file._id, file.filename)}
+                        >
+                          <span class="material-symbols-outlined">
+                            download
+                          </span>
+                        </button>
+                      </div>
+                    </div>
+                  );
+                }
+                return null;
+              })}
           </div>
         )}
-       
       </div>
       <div className="physics-sect">
         <div className="file-header">Physics</div>
-          {loading ? (
-            <LoadingSpinner />
-          ) : (
-            <div>
-              {files &&
-          files.map((file) => {
-            if (file.category === "Physics") {
-              return (
-                <div className="file" key={file._id}>
-                <div className="file-sub-cont">
-                  <h3>{file.filename.length > maxLength? file.filename.slice(0,maxLength -3) + '...':file.filename}</h3>
-                  <button
-                    className="download-btn"
-                    onClick={() =>
-                      downloadFile(file._id, file.filename)
-                    }
-                  >
-                    <span class="material-symbols-outlined">
-                      download
-                    </span>
-                  </button>
-                </div>
-              </div>
-              );
-            }
-            return null;
-          })}
-            </div>
-          )}
-        
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <div>
+            {files &&
+              files.map((file) => {
+                if (file.category === "Physics") {
+                  return (
+                    <div className="file" key={file._id}>
+                      <div className="file-sub-cont">
+                        <h3>
+                          {file.filename.length > maxLength
+                            ? file.filename.slice(0, maxLength - 3) + "..."
+                            : file.filename}
+                        </h3>
+                        <button
+                          className="download-btn"
+                          onClick={() => downloadFile(file._id, file.filename)}
+                        >
+                          <span class="material-symbols-outlined">
+                            download
+                          </span>
+                        </button>
+                      </div>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+          </div>
+        )}
       </div>
 
       <div className="maths-sect">
         <div className="file-header">Mathematics</div>
-          {loading ? (
-            <LoadingSpinner />
-          ) : (
-            <div>
-        {files &&
-          files.map((file) => {
-            if (file.category === "Mathematics") {
-              return (
-                <div className="file" key={file._id}>
-                <div className="file-sub-cont">
-                  <h3>{file.filename.length > maxLength? file.filename.slice(0,maxLength -3) + '...':file.filename}</h3>
-                  <button
-                    className="download-btn"
-                    onClick={() =>
-                      downloadFile(file._id, file.filename)
-                    }
-                  >
-                    <span class="material-symbols-outlined">
-                      download
-                    </span>
-                  </button>
-                </div>
-              </div>
-              );
-            }
-            return null;
-          })}
-            </div>
-          )}
-        
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <div>
+            {files &&
+              files.map((file) => {
+                if (file.category === "Mathematics") {
+                  return (
+                    <div className="file" key={file._id}>
+                      <div className="file-sub-cont">
+                        <h3>
+                          {file.filename.length > maxLength
+                            ? file.filename.slice(0, maxLength - 3) + "..."
+                            : file.filename}
+                        </h3>
+                        <button
+                          className="download-btn"
+                          onClick={() => downloadFile(file._id, file.filename)}
+                        >
+                          <span class="material-symbols-outlined">
+                            download
+                          </span>
+                        </button>
+                      </div>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+          </div>
+        )}
       </div>
 
       <div className="biology-sect">
@@ -186,70 +188,69 @@ const CourseSlides = () => {
         ) : (
           <div>
             {files &&
-          files.map((file) => {
-            if (file.category === "Biology") {
-              return (
-                <div className="file" key={file._id}>
-                        <div className="file-sub-cont">
-                          <h3>{file.filename.length > maxLength? file.filename.slice(0,maxLength -3) + '...':file.filename}</h3>
-                          <button
-                            className="download-btn"
-                            onClick={() =>
-                              downloadFile(file._id, file.filename)
-                            }
-                          >
-                            <span class="material-symbols-outlined">
-                              download
-                            </span>
-                          </button>
-                        </div>
+              files.map((file) => {
+                if (file.category === "Biology") {
+                  return (
+                    <div className="file" key={file._id}>
+                      <div className="file-sub-cont">
+                        <h3>
+                          {file.filename.length > maxLength
+                            ? file.filename.slice(0, maxLength - 3) + "..."
+                            : file.filename}
+                        </h3>
+                        <button
+                          className="download-btn"
+                          onClick={() => downloadFile(file._id, file.filename)}
+                        >
+                          <span class="material-symbols-outlined">
+                            download
+                          </span>
+                        </button>
                       </div>
-
-              );
-            }
-            return null;
-          })}
+                    </div>
+                  );
+                }
+                return null;
+              })}
           </div>
         )}
-        
       </div>
 
       <div className="english-sect">
         <div className="file-header">English</div>
-        {loading ? ( 
+        {loading ? (
           <LoadingSpinner />
         ) : (
           <div>
             {files &&
-          files.map((file) => {
-            if (file.category === "English") {
-              return (
-                <div className="file" key={file._id}>
-                <div className="file-sub-cont">
-                  <h3>{file.filename.length > maxLength? file.filename.slice(0,maxLength -3) + '...':file.filename}</h3>
-                  <button
-                    className="download-btn"
-                    onClick={() =>
-                      downloadFile(file._id, file.filename)
-                    }
-                  >
-                    <span class="material-symbols-outlined">
-                      download
-                    </span>
-                  </button>
-                </div>
-              </div>
-
-              );
-            }
-            return null;
-          })}
+              files.map((file) => {
+                if (file.category === "English") {
+                  return (
+                    <div className="file" key={file._id}>
+                      <div className="file-sub-cont">
+                        <h3>
+                          {file.filename.length > maxLength
+                            ? file.filename.slice(0, maxLength - 3) + "..."
+                            : file.filename}
+                        </h3>
+                        <button
+                          className="download-btn"
+                          onClick={() => downloadFile(file._id, file.filename)}
+                        >
+                          <span class="material-symbols-outlined">
+                            download
+                          </span>
+                        </button>
+                      </div>
+                    </div>
+                  );
+                }
+                return null;
+              })}
           </div>
         )}
-        
       </div>
       <PopUp open={popupState} />
-     
     </div>
   );
 };
